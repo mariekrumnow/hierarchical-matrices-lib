dd8ba5edbec67f3dce6482331dccb20b9a5983af
mariekrumnow <93008705+mariekrumnow@users.noreply.github.com>
2022-05-28 21:50:42 +0200

    Preparation for class-overarching functions

--- OuterProductBlock.cpp
+++ OuterProductBlock.cpp
@@ -11,4 +11,36 @@ Block<datatype, xDim, yDim>& OuterProductBlock<datatype, xDim, yDim>::coarse(){
 
 }
 
-//Verrechnungfkten
+//---------------------------------------------------------------------------------------
+
+template <class datatype, unsigned int xDim, unsigned int yDim>
+OuterProductBlock<datatype, xDim, yDim>& OuterProductBlock<datatype, xDim, yDim>::operator+( const OuterProductBlock<datatype, xDim, yDim>& addedBlock ){
+
+}
+
+template <class datatype, unsigned int xDim, unsigned int yDim>
+OuterProductBlock<datatype, xDim, yDim>& OuterProductBlock<datatype, xDim, yDim>::operator+( const EntrywiseBlock<datatype, xDim, yDim>& addedBlock ){
+
+}
+
+template <class datatype, unsigned int xDim, unsigned int yDim>
+OuterProductBlock<datatype, xDim, yDim>& OuterProductBlock<datatype, xDim, yDim>::operator+( const HierarchicalMatrix<datatype, xDim>& addedBlock ){
+
+}
+
+//---------------------------------------------------------------------------------------
+
+template <class datatype, unsigned int xDim, unsigned int yDim>
+OuterProductBlock<datatype, xDim, yDim>& OuterProductBlock<datatype, xDim, yDim>::operator*( const OuterProductBlock<datatype, xDim, yDim>& multBlock ){
+
+}
+
+template <class datatype, unsigned int xDim, unsigned int yDim>
+OuterProductBlock<datatype, xDim, yDim>& OuterProductBlock<datatype, xDim, yDim>::operator*( const EntrywiseBlock<datatype, xDim, yDim>& multBlock ){
+
+}
+
+template <class datatype, unsigned int xDim, unsigned int yDim>
+OuterProductBlock<datatype, xDim, yDim>& OuterProductBlock<datatype, xDim, yDim>::operator*( const HierarchicalMatrix<datatype, xDim>& multBlock ){
+
+}
