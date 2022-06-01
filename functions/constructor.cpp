9439dfca3fefceb51eaaf15d95ae5ae156c0bee8
mariekrumnow <93008705+mariekrumnow@users.noreply.github.com>
2022-06-01 18:29:34 +0200

    3d array tests for constructor

--- functions/constructor.cpp
+++ functions/constructor.cpp
@@ -16,22 +16,32 @@ HierarchicalMatrix<datatype,dim>::HierarchicalMatrix(datatype (&originalMatrix)[
       // (Attr dass dafür true gesetzt, sonst default =false)
       // Falls nein: Fehler, müsste eigentlich als OP/EW gespeichert werden
 
-      // Nächstes Level im algebraischen ermitteln (aktuelle Range von Zeilen/Spalten je einmal aufteilen)
-      // Für jede entstehende neue Aufteilungen prüfen, ob sich diese weiter aufteilen lassen --> minimal cluster size beachten
-      // Falls ja: matrix für den Quadranten = HierarchicalMatrix-Konstruktor mit neuer Dim/Startspalten usw
-      // Falls nein: matrix für den Quadranten = OP(ad) oder EW-Konstruktor(non) je nach Admissibilty?
+      // Nächstes Level im algebraischen CLusterbaum ermitteln (aktuelle range von Zeilen/Spalten je einmal aufteilen)
 
+      unsigned int splitI;
+      unsigned int splitJ;
+
+      // https://stackoverflow.com/questions/2178909/how-to-initialize-3d-array-in-c
+      unsigned int newIndices[2][2][2];
 
     for (int i=0; i<2; i++) {
         for (int j=0; j<2; j++) {
-            if (/*aufteilbar*/) {
+            bool aufteilbar = true; //Lieber false init?
+            
+            // Für jede entstehende neue Aufteilungen prüfen, ob sich diese weiter aufteilen lassen --> minimal cluster size beachten
+            // Falls ja: matrix für den Quadranten = HierarchicalMatrix-Konstruktor mit neuer Dim/Startspalten usw
+            // Falls nein: matrix für den Quadranten = OP(ad) oder EW-Konstruktor(non) je nach Admissibilty?
+
+            if (aufteilbar) {
                 matrix[i][j] = new HierarchicalMatrix<datatype, /*neue dim*/>(/*abgekappte Daten*/);
             }
             else {
+                bool admissible = true;
+
                 // k = Rang der Matrix? Weil Rang = Maximale Anzahl linear unabhängiger Zeilen/Spalten = k (S.10)
                 // Wenn k(xDim+yDim) < xDim*yDim: OuterProduct-Form benutzen
-                
-                if (/*admissible*/) {
+
+                if (admissible) {
                     matrix[i][j] = new OuterProductBlock<datatype, /*neue dim x2*/>(/*abgekappte Daten*/);
                 }
                 else {
--- test.cc
+++ test.cc
@@ -1,27 +1,31 @@
-#include "Exceptions.hpp"
-
-#include "EntrywiseBlock.hpp"
-#include "OuterProductBlock.hpp"
-#include "HierarchicalMatrix.hpp"
-
-#include "EntrywiseBlock.cpp"
-#include "OuterProductBlock.cpp"
-
-#include "functions/addition.cpp"
-#include "functions/coarse.cpp"
-#include "functions/constructor.cpp"
-#include "functions/inversion.cpp"
-#include "functions/luDecomposition.cpp"
-#include "functions/matrixMatrixMult.cpp"
-#include "functions/matrixVectorMult.cpp"
+// #include "Exceptions.hpp"
+//
+// #include "EntrywiseBlock.hpp"
+// #include "OuterProductBlock.hpp"
+// #include "HierarchicalMatrix.hpp"
+//
+// #include "EntrywiseBlock.cpp"
+// #include "OuterProductBlock.cpp"
+//
+// #include "functions/addition.cpp"
+// #include "functions/coarse.cpp"
+// #include "functions/constructor.cpp"
+// #include "functions/inversion.cpp"
+// #include "functions/luDecomposition.cpp"
+// #include "functions/matrixMatrixMult.cpp"
+// #include "functions/matrixVectorMult.cpp"
 
 #include <iostream>
 
 int main(int argc, char** argv){
-      double daten[2][2] = { {1.1, 2.2}, {3.3, 4.4} };
-      HierarchicalMatrix<double, 2> * exampleBlock = new HierarchicalMatrix<double, 2>(daten);
+      // double daten[2][2] = { {1.1, 2.2}, {3.3, 4.4} };
+      // HierarchicalMatrix<double, 2> * exampleBlock = new HierarchicalMatrix<double, 2>(daten);
+      //
+      // std::cout << "Warnungsvermeider: " << sizeof(exampleBlock); // Vermeidet Warning, dass exampleBlock unbenutzt ist
 
-      std::cout << "Warnungsvermeider: " << sizeof(exampleBlock); // Vermeidet Warning, dass exampleBlock unbenutzt ist
+      unsigned int newIndices[2][2][2] = { { {000, 001}, {010, 011} },
+                                        { {100, 101}, {110, 111} } };
+    std::cout << newIndices[1][0][0];
 
       return 0;
 }
